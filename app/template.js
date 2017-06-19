60f53a13f6289dce7083d0603dc842be8bd1a6f3
Marcos <mfernandmx@alumnos.unex.es>
2017-06-19 18:29:05 +0200

    Multivalued attributes and relations displayed

--- app/template.js
+++ app/template.js
@@ -18,12 +18,32 @@ function setContentPug(title, uri, types, literals, relations, typedLiterals, re
 
     var ele;
 
+    var literalsAux = [];
+    var typedLiteralsAux = [];
+    var relationsAux = [];
+    var reverseRelationsAux = [];
+
+    var found, i;
+
+
     // Procesamos los valores que sean literales
     for (element in literals) {
         //var ele  = {type: literals[element]["p"].value, value: literals[element]["o"].value};
 
         ele  = {relation: literals[element].relation, value: literals[element].value};
         literalsValues.push(ele);
+
+        found = false;
+        for(i = 0; i < literalsAux.length; i++) {
+            if (literalsAux[i].url == literals[element].relation.url) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found){
+            literalsAux.push(literals[element].relation);
+        }
     }
 
     // Procesamos los valores que sean literales tipados
@@ -32,10 +52,19 @@ function setContentPug(title, uri, types, literals, relations, typedLiterals, re
         replaceType(typedLiterals[element].value);
         ele  = {relation: typedLiterals[element].relation, value: typedLiterals[element].value};
         typedLiteralsValues.push(ele);
-        //console.log(typedLiterals[element]["o"].datatype);
-    }
 
-    var relationsAux = [];
+        found = false;
+        for(i = 0; i < typedLiteralsAux.length; i++) {
+            if (typedLiteralsAux[i].url == typedLiterals[element].relation.url) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found){
+            typedLiteralsAux.push(typedLiterals[element].relation);
+        }
+    }
 
     // Procesamos los valores que sean relaciones
     for (element in relations) {
@@ -50,8 +79,8 @@ function setContentPug(title, uri, types, literals, relations, typedLiterals, re
         ele  = {relation: relations[element].relation, value: relations[element].value};
         relationsValues.push(ele);
 
-        var found = false;
-        for(var i = 0; i < relationsAux.length; i++) {
+        found = false;
+        for(i = 0; i < relationsAux.length; i++) {
             if (relationsAux[i].url == relations[element].relation.url) {
                 found = true;
                 break;
@@ -75,6 +104,18 @@ function setContentPug(title, uri, types, literals, relations, typedLiterals, re
 
         ele  = {relation: reverseRelations[element].relation, value: reverseRelations[element].value};
         reverseRelationsValues.push(ele);
+
+        found = false;
+        for(i = 0; i < reverseRelationsAux.length; i++) {
+            if (reverseRelationsAux[i].url == reverseRelations[element].relation.url) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found){
+            reverseRelationsAux.push(reverseRelations[element].relation);
+        }
     }
 
     const compiledFunction = pug.compileFile('./pug/content.pug');
@@ -89,7 +130,11 @@ function setContentPug(title, uri, types, literals, relations, typedLiterals, re
         relations: relationsValues,
         reverseRelations: reverseRelationsValues,
 
-        relationsAux: relationsAux
+        literalsAux: literalsAux,
+        typedLiteralsAux: typedLiteralsAux,
+        relationsAux: relationsAux,
+        reverseRelationsAux: reverseRelationsAux
+
     });
 
     // Render a set of data
